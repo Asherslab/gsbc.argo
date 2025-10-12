6406f989edb7917141c2530e8a8f5ede0f210da7
Asherslab <Asherslab@users.noreply.github.com>
2025-10-12 21:36:07 +1000

    stuff

--- bootstrap-argo.sh
+++ bootstrap-argo.sh
@@ -1,2 +1,4 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install --version 7.6.8 --namespace argo-cd --create-namespace -f ./apps/argo-cd/values.yaml argo-cd argo/argo-cd
+
+kubectl apply -f ./aoa.yaml
